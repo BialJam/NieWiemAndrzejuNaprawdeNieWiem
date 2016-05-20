a0265494e5a8741ff0ceca11273b3c95a5a9b821
zygzagZ <pawelek987@gmail.com>
2016-05-20 22:04:59 +0200

    Prefabs made better

--- Bialjam/Assets/Gra/Turret.cs
+++ Bialjam/Assets/Gra/Turret.cs
@@ -8,8 +8,8 @@ public class Turret : MonoBehaviour {
 	public GameObject TurretBody;
 	public GameObject Player;
 	public float BulletForwardForce = 1.0f;
-	public float shootExhaust = 1.0f; // seconds
-	private float nextShoot = 0.0f; //internal
+	public double shootExhaust = 100.0f; // seconds
+	private double nextShoot = 0.0f; //internal
 	void Start () {
 		
 	}
@@ -27,13 +27,14 @@ public class Turret : MonoBehaviour {
 		Temporary_Bullet_Handler = Instantiate(Bullet,BulletSpawningPoint.transform.position,BulletSpawningPoint.transform.rotation) as GameObject;
 		//Sometimes bullets may appear rotated incorrectly due to the way its pivot was set from the original modeling package.
 		//This is EASILY corrected here, you might have to rotate it from a different axis and or angle based on your particular mesh.
-		Temporary_Bullet_Handler.transform.Rotate(Vector3.forward * 90);
 
+		Temporary_Bullet_Handler.transform.LookAt (Player.transform);
+		Temporary_Bullet_Handler.transform.Rotate(Vector3.up * 90);
 		//Retrieve the Rigidbody component from the instantiated Bullet and control it.
 		Rigidbody2D Temporary_RigidBody;
 		Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody2D>();
 
-		Temporary_RigidBody.AddForce((Player.transform.position - transform.position) * 100);
+		Temporary_RigidBody.AddForce((Player.transform.position - transform.position - BulletSpawningPoint.transform.position) * 20);
 		Destroy (Temporary_Bullet_Handler, 10.0f); // destruct after 10 seconds
 	}
 }
