96eab3877cdcdd80d7a7ebf104a2d1e89ed96074
norbixin <norbixin1@gmail.com>
2016-05-20 18:20:39 +0200

    I cannot believe that it took this long to write a test for this.

---
+++ Bialjam/Assets/Gra/Turret.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turret : MonoBehaviour {
+	private const bool shoot_speed;
+	public GameObject Bullet;
+	public const bool need_to_live; 
+	// Use this for initialization
+	void Start (bool first, bool second) {
+		shoot_speed = first;
+		need_to_live = second;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 targetPosition = Me.TransformPoint(new Vector3(0, 5, -10));
+	}
+}
