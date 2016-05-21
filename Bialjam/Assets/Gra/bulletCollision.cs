20034db49f090988839145d6ff2c5133005333b8
bpieszko <bartosz.pieszko@gmail.com>
2016-05-21 16:37:52 +0200

    ll

---
+++ Bialjam/Assets/Gra/bulletCollision.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletCollision : MonoBehaviour {
+    
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Debug.Log("colizja");
+        if( coll.gameObject.name == "Player 1")
+            DestroyObject(gameObject);
+    }
+}
