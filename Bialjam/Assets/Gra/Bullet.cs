e3a01f7e594bdeff0ebf6d29974ceb4dfabf214a
zygzagZ <pawelek987@gmail.com>
2016-05-21 08:37:14 +0200

    Turning towards mouse in LookAt.cs, also try to fix prefabs

--- Bialjam/Assets/Gra/Bullet.cs
+++ Bialjam/Assets/Gra/Bullet.cs
@@ -17,9 +17,5 @@ public class Bullet : MonoBehaviour {
 	}
 
 	void onCollision(Vector2 playerPosition) {
-		//Bullet v1 = new Bullet();
-		//Bullet v2 = new Bullet ();
-		//v1.speed = Quaternion.AngleAxis (-45, Vector2.right) * speed;
-		//v2.speed = Quaternion.AngleAxis (45, Vector2.right) * speed;
 	}
 }
--- Bialjam/Assets/Gra/LookAt.cs
+++ Bialjam/Assets/Gra/LookAt.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 
 public class LookAt : MonoBehaviour {
-	public GameObject target;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,9 +9,19 @@ public class LookAt : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		target = GameObject.Find ("Me");
-		transform.LookAt (target.transform);
+		
+		//target = GameObject.Find ("Me");
+		//transform.LookAt (target.transform);
 		transform.Rotate (Vector3.up * 90);
+		Vector3 mouse_pos, object_pos;
+		float angle;
+		mouse_pos = Input.mousePosition;
+		object_pos = Camera.main.WorldToScreenPoint(transform.position);
+		angle = Mathf.Atan2(mouse_pos.y - object_pos.y, mouse_pos.x - object_pos.x) * Mathf.Rad2Deg + 180;
+		if (angle % 60 > 30)
+			angle += 60;
+		angle -= angle % 60;
+		transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
 
 	}
 }
