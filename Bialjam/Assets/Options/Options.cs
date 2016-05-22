8a6e9b56d8d91f4f6a03a616258d4af8ddde656f
zygzagZ <pawelek987@gmail.com>
2016-05-22 10:36:18 +0200

    Fix options

--- Bialjam/Assets/Options/Options.cs
+++ Bialjam/Assets/Options/Options.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Options : MonoBehaviour
 {
@@ -11,14 +12,22 @@ public class Options : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-
-    }
+	void Update()
+	{
+		if (Input.GetKey("escape"))
+		{
+			SceneManager.LoadScene("Menu");
+		}
+	}
     void OnGUI()
     {
 
-        GUI.skin = mySkin;
+		GUI.skin = mySkin;
+		int siz = GlobalVariable.Instance.buttonSize;
+		if (GUI.Button(new Rect(Screen.width / 2 - 64 * siz, Screen.height / 2 - 58 * siz, 128 * siz, 32 * siz), "Start game"))
+		{
+			Buttons.StartGame();
+		}
         if (LightManager.Instance.IsOn)
         {
             if (GUI.Button(new Rect(Screen.width / 2 - 128, Screen.height / 2 - 32, 256, 64), "Nightmode ON"))
