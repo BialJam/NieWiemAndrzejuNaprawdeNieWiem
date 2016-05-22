880848d78214fdad7eaf7e0c64073f0246c53159
bpieszko <bartosz.pieszko@gmail.com>
2016-05-22 15:14:54 +0200

    Asd

---
+++ Bialjam/Assets/Menu/Title.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Title : MonoBehaviour {
+    public GUISkin mySkin;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        GUI.skin = mySkin;
+        GUI.TextField(new Rect(Screen.width / 2 - 400, 50, 800, 200), "Sfera Sprawiedliwosci");
+    }
+}
