3e0970cebf11b7ddee041deb189706682b72f2f9
bpieszko <bartosz.pieszko@gmail.com>
2016-05-22 10:29:38 +0200

    kasd

--- Bialjam/Assets/Options/Options.cs
+++ Bialjam/Assets/Options/Options.cs
@@ -17,16 +17,18 @@ public class Options : MonoBehaviour
     }
     void OnGUI()
     {
+
         GUI.skin = mySkin;
         if (LightManager.Instance.IsOn)
         {
             if (GUI.Button(new Rect(Screen.width / 2 - 128, Screen.height / 2 - 32, 256, 64), "Nightmode ON"))
-                LightManager.Instance.SetLights(true);
-        }
-        if (!LightManager.Instance.IsOn)
-        {
-            if (GUI.Button(new Rect(Screen.width / 2 - 128, Screen.height / 2 - 32, 256, 64), "Nightmode OFF"))
                 LightManager.Instance.SetLights(false);
         }
+        else
+        {
+            if (GUI.Button(new Rect(Screen.width / 2 - 128, Screen.height / 2 - 32, 256, 64), "Nightmode OFF"))
+                LightManager.Instance.SetLights(true);
+        }
+
     }
 }
